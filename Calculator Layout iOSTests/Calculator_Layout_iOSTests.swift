34120af0a3d7ac08fb6cc39a4b77437a72c58cfd
FrancaTM <francatm@outlook.com>
2020-09-29 22:37:42 -0300

    Initial Commit

---
+++ Calculator Layout iOSTests/Calculator_Layout_iOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  Calculator_Layout_iOSTests.swift
+//  Calculator Layout iOSTests
+//
+//  Created by Tulio Marcos Franca on 29/09/20.
+//
+
+import XCTest
+@testable import Calculator_Layout_iOS
+
+class Calculator_Layout_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
